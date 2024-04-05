6d81eb6ec09009dc504bec27be780f7b91a63f8f
kagerka <kagerka@gmail.com>
2024-04-05 14:44:55 +0300

    feat: add solution to getFunctionBody task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -32,8 +32,14 @@ function getCurrentFunctionName() {
  *   getFunctionBody(hiHello) => "function hiHello() { console.log('hello world'); }"
  *
  */
-function getFunctionBody(/* func */) {
-  throw new Error('Not implemented');
+function getFunctionBody(func) {
+  let result = '';
+  if (typeof func === 'undefined') {
+    result = '';
+  } else {
+    result = func.toString();
+  }
+  return result;
 }
 
 /**
