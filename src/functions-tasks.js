e93cefd71a2978ebc4d783729b327dc8a5bc48a7
kagerka <kagerka@gmail.com>
2024-04-10 14:14:44 +0300

    feat: add partial solution to logger task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -179,8 +179,13 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    logFunc(`${func.name}(${[...args]}) starts`);
+    const fn = func(`${args}`);
+    logFunc(`${func.name}(${[...args]}) ends`);
+    return fn;
+  };
 }
 
 /**
